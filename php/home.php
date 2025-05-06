073eb8643566d1f04c0b033e2d22c2fcbe86c8fb
Gabriel H. <141324085+gabrielvicentm@users.noreply.github.com>
2025-05-06 14:46:43 -0300

    Update home.php

--- php/home.php
+++ php/home.php
@@ -19,7 +19,7 @@ if (!isset($_SESSION['user_id'])) {
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Home</title>
-  <link rel="stylesheet" href="../css/home.css">
+  <link rel="stylesheet" href="../css/diario.css">
 
 
 </head>
@@ -27,7 +27,7 @@ if (!isset($_SESSION['user_id'])) {
   <div class="sidebar">
       <div class="logo" style="color: orange;">LOGO LINDA</div>
     <a href="home.php"><img src="https://img.icons8.com/ios-filled/24/ffffff/home.png"/><span>Página Inicial</span></a>
-    <a href="diario.html"><img src="https://img.icons8.com/ios-filled/24/ffffff/dumbbell.png"/><span>Diário de Treino</span></a>
+    <a href="diario.php"><img src="https://img.icons8.com/ios-filled/24/ffffff/dumbbell.png"/><span>Diário de Treino</span></a>
     <a href="pesquisa.php"><img src="https://img.icons8.com/ios-filled/24/ffffff/search.png"/><span>Pesquisa</span></a>
     <a href="reels.php"><img src="https://img.icons8.com/ios-filled/24/ffffff/film-reel.png"/><span>Vídeos Curtos</span></a>
     <a href="postar.php"><img src="https://img.icons8.com/ios-filled/24/ffffff/plus-math.png"/><span>Postar</span></a>
@@ -41,6 +41,13 @@ if (!isset($_SESSION['user_id'])) {
   <div class="content"> 
     <h1>Bem-vindo à Página Inicial</h1>
     <p>Em breve, seu feed vai aparecer aqui!(eu espero)</p>
+    <div class="button-container">
+        <a href="diario_form.php" class="diary-button">🏋️‍♂️ <span>Adicionar Treino</span></a>
+        <a href="ver_treinos" class="diary-button">📅 <span>Histórico</span></a>
+        <a href="estatisticas.html" class="diary-button">📊 <span>Estatísticas</span></a>
+        <a href="metas.html" class="diary-button">🎯 <span>Metas</span></a>
+        <a href="progresso.html" class="diary-button">📸 <span>Progresso Visual</span></a>
+      </div>
   </div>
 </body>
 </html>
