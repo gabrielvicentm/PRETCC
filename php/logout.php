dc1c22610d085816530f39e60f2dba957dee40f5
Richard <richardmarinho360@gmail.com>
2025-05-11 20:04:05 -0300

    Delete php directory

--- php/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start(); // Inicia a sessão
-session_unset(); // Limpa todas as variáveis de sessão
-session_destroy(); // Destroi a sessão
-
-header("Location: ../html/index.html"); // Redireciona para o login
-exit();
-?>
