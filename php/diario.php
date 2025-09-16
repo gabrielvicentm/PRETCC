2a32d6624b73e4c1720d794c37503abe9364c8f4
gabrielvicentm <gabrielvicentee068@gmail.com>
2025-09-16 14:13:35 -0300

    ODEIO ESSA MATÉRIA

--- php/diario.php
+++ php/diario.php
@@ -24,7 +24,7 @@ if (!isset($_SESSION['user_id'])) {
 
 </head>
 <body>
-  <div class="sidebar">
+  <div class="sidebar"> 
        <div class="logo">
   <img src="../img/logo.png" alt="Logo" style="height: 100px;">
 </div>
@@ -45,10 +45,7 @@ if (!isset($_SESSION['user_id'])) {
     <p>Em breve, seu feed vai aparecer aqui!(eu espero)</p>
     <div class="button-container">
         <a href="diario_form.php" class="diary-button">🏋️‍♂️ <span>Adicionar Treino</span></a>
-        <a href="ver_treinos" class="diary-button">📅 <span>Histórico</span></a>
-        <a href="estatisticas.html" class="diary-button">📊 <span>Estatísticas</span></a>
-        <a href="metas.html" class="diary-button">🎯 <span>Metas</span></a>
-        <a href="progresso.html" class="diary-button">📸 <span>Progresso Visual</span></a>
+        <a href="ver_treinos" class="diary-button">📅 <span>Meus Treinos</span></a>
       </div>
       
   </div>
